ef5c52db8b4c7a17e6dbf60badaa0920a7f094d5
denise zahreddine <denise.zahreddine@gmail.com>
2025-11-14 11:28:11 +0100

    - Separation db Connection afin de pouvoir l’utiliser dans plusieurs DAO

--- src/main/java/com/project/utilisateur/DAOUtilisateur.java
+++ src/main/java/com/project/utilisateur/DAOUtilisateur.java
@@ -11,11 +11,6 @@ import java.util.ArrayList;
 public class DAOUtilisateur  {
     private Connection conn;
 
-    private static final Dotenv dotenv = Dotenv.load();
-    private static final String URL = dotenv.get("DB_URL");
-    private static final String USER = dotenv.get("DB_USER");
-    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
-
     public DAOUtilisateur() {
         this.conn = DbConnection.getConnection();
     }
